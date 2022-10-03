ab5f968abc314eb14913d12701c793b903e42cad
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-10-03 10:56:59 +0200

    unnecessary components removed

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import CTA from './CTA'
-import "./header.css"
-import ME from "../../assets/random_ME.png"
-import HeaderSocial from './HeaderSocial'
+import React from "react";
+import CTA from "./CTA";
+import "./header.css";
+import ME from "../../assets/random_ME.png";
+import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
   return (
@@ -10,19 +10,19 @@ const Header = () => {
       <div className="container header__container">
         <h3>Hello, I'm</h3>
         <h1>Samuel Li Wang</h1>
-        <h5>
-          Fullstack Developer
-        </h5>
+        <h5>Web Developer</h5>
         <CTA />
         <HeaderSocial />
         <div className="me">
           <img src={ME} alt="my portrait photo" />
         </div>
 
-        <a href='#contact' className='scroll__down'>Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
