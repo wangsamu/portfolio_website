774d6411700a9e45a00ff06c84d8887caccc44f2
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-10-04 09:49:50 +0200

    social media links modified

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,38 +1,44 @@
-import React from 'react'
-import "./footer.css"
-import { BsLinkedin, BsGithub } from "react-icons/bs"
-import { SiTwitter } from "react-icons/si"
-
+import React from "react";
+import "./footer.css";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { SiTwitter } from "react-icons/si";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>LOGO HERE</a>
+      <a href="#" className="footer__logo">
+        SAMUEL LI WANG
+      </a>
       <div className="footer__permalinks">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#experience">Experience</a>
-          <a href="#services">Services</a>
-          <a href="#portfolio">Portfolio</a>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            {/* <a href="#experience">Experience</a>
+            <a href="#services">Services</a> */}
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
       </div>
 
-
       <div className="footer__socials">
-        <a href="https://github.com"><BsGithub /></a>
-        <a href="https://linkedin.com"><BsLinkedin /></a>
-        <a href="https://twitter.com"><SiTwitter /></a>
+        <a href="https://github.com/wangsamu">
+          <BsGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/wangsamu/">
+          <BsLinkedin />
+        </a>
+        {/* <a href="https://twitter.com">
+          <SiTwitter />
+        </a> */}
       </div>
 
       <div className="footer__copyrights">
-        <small>&copy; wangsamu.com  ||  All rights reserved.</small>
+        <small>&copy; wangsamu.com || All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/header/HeaderSocial.jsx
+++ src/Components/header/HeaderSocial.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { BsLinkedin, BsGithub } from "react-icons/bs"
-import { SiDribbble } from "react-icons/si"
+import React from "react";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { SiDribbble } from "react-icons/si";
 
 const HeaderSocial = () => {
   return (
-    <div className='header__socials'>
-        <a href='linkedin.com' target="_blank"><BsLinkedin /></a>
-        <a href='github.com' target="_blank"><BsGithub /></a>
-        <a href='dribble.com' target="_blank"><SiDribbble /></a>
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/wangsamu/" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/wangsamu" target="_blank">
+        <BsGithub />
+      </a>
+      {/* <a href="dribble.com" target="_blank">
+        <SiDribbble />
+      </a> */}
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocial
+export default HeaderSocial;
