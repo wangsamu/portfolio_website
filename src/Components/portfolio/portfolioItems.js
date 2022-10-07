4c17d1277d17953fd3072f4c66c2150a466cad74
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-10-07 11:17:51 +0200

    Portfolio item images aspect ratio edited

---
+++ src/Components/portfolio/portfolioItems.js
@@ -0,0 +1,53 @@
+//import portfolio images here
+import ARKANOID from "../../assets/arkanoid.png";
+import CALCULATOR from "../../assets/calculator.png";
+import CHATAPP from "../../assets/chatapp.png";
+import DRUM_MACHINE from "../../assets/drum_machine.png";
+import ECOMMERCE from "../../assets/ecommerce.png";
+import QUOTE_MACHINE from "../../assets/quote_machine.png";
+
+export const portfolioItems = [
+  {
+    id: 1,
+    //insert portfolio image or gif here
+    image: ARKANOID,
+    title: "React Arkanoid",
+    github: "https://github.com/wangsamu/react-arkanoid",
+    demo: "https://github.com/wangsamu/react-arkanoid",
+  },
+  {
+    id: 2,
+    image: ECOMMERCE,
+    title: "React e-commerce",
+    github: "https://github.com/wangsamu/ecommerce_react_firebase",
+    demo: "https://ecommreactfirebase.netlify.app/",
+  },
+  {
+    id: 3,
+    image: CHATAPP,
+    title: "Chat App",
+    github: "https://github.com/wangsamu/chatapp-react-firebase",
+    demo: "https://chatapp.wangsamu.com/",
+  },
+  {
+    id: 4,
+    image: CALCULATOR,
+    title: "React Calculator",
+    github: "https://codepen.io/wangsamu/pen/QWmPOwN",
+    demo: "https://codepen.io/wangsamu/full/QWmPOwN",
+  },
+  {
+    id: 5,
+    image: DRUM_MACHINE,
+    title: "Drum Machine",
+    github: "https://codepen.io/wangsamu/pen/MWVZXVO",
+    demo: "https://codepen.io/wangsamu/full/MWVZXVO",
+  },
+  {
+    id: 6,
+    image: QUOTE_MACHINE,
+    title: "Random Quote Machine",
+    github: "https://codepen.io/wangsamu/pen/rNdqxew",
+    demo: "https://codepen.io/wangsamu/full/rNdqxew",
+  },
+];
