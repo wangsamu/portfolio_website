c1eba163caa05215356a888410a31eb4fab1fc90
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-11-01 20:01:17 +0100

    Old CSS files modified, migration to styled-components pending

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,28 +1,27 @@
-import React from "react";
-import "./portfolio.css";
-import { portfolioItems } from "./portfolioItems";
+import React from 'react';
+import './portfolio.css';
+import { portfolioItems } from './portfolioItems';
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">
-      <h5>My projects</h5>
-      <h2>Portfolio</h2>
+    <section id='portfolio'>
+      <h2>My Projects</h2>
 
-      <div className="container portfolio__container">
+      <div className='container portfolio__container'>
         {portfolioItems.map(
           ({ id, image, title, description, github, demo }) => {
             return (
-              <article className="portfolio__item" key={id}>
-                <div className="portfolio__item-image">
+              <article className='portfolio__item' key={id}>
+                <div className='portfolio__item-image'>
                   <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
                 <p>{description}</p>
-                <div className="portfolio__item-cta">
-                  <a href={github} className="btn" target="_blank">
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn btn-primary' target='_blank'>
                     Github
                   </a>
-                  <a href={demo} className="btn btn-primary" target="_blank">
+                  <a href={demo} className='btn btn-primary' target='_blank'>
                     Live Demo
                   </a>
                 </div>
