b4dbc6d8b338cc38b7fc487deb91360b39655d54
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-11-03 15:46:44 +0100

    ContactStyled added, projects styling modified

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import "./contact.css";
-import { HiOutlineMail } from "react-icons/hi";
-import { RiMessengerLine } from "react-icons/ri";
-import { BsGithub, BsLinkedin, BsWhatsapp } from "react-icons/bs";
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
+import React from 'react';
+import './contact.css';
+import { HiOutlineMail } from 'react-icons/hi';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsGithub, BsLinkedin, BsWhatsapp } from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ContactContainer } from './ContactStyled';
 
 const Contact = () => {
   const form = useRef();
@@ -14,10 +15,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_hhbbu7z",
-        "template_400x2du",
+        'service_hhbbu7z',
+        'template_400x2du',
         form.current,
-        "SAQP_Tp4ekMBPzpWa"
+        'SAQP_Tp4ekMBPzpWa'
       )
       .then(
         (result) => {
@@ -31,54 +32,53 @@ const Contact = () => {
   };
 
   return (
-    <section id="contact">
-      <h5>Get In Touch</h5>
+    <section id='contact'>
       <h2>Contact</h2>
 
-      <div className="container contact__container">
-        <div className="contact__options">
+      <ContactContainer>
+        <div className='contact__options'>
           {/* LinkedIn */}
-          <article className="contact__option">
-            <BsLinkedin className=".contact__option-icon" />
+          <article className='contact__option'>
+            <BsLinkedin className='.contact__option-icon' />
             <h4>LinkedIn</h4>
-            <a href="https://www.linkedin.com/in/wangsamu/" target="_blank">
-              Connect{" "}
+            <a href='https://www.linkedin.com/in/wangsamu/' target='_blank'>
+              Connect{' '}
             </a>
           </article>
           {/* Github */}
-          <article className="contact__option">
-            <BsGithub className=".contact__option-icon" />
+          <article className='contact__option'>
+            <BsGithub className='.contact__option-icon' />
             <h4>Github</h4>
             {/* <h5>@wangsamu</h5> */}
-            <a href="https://github.com/wangsamu" target="_blank">
+            <a href='https://github.com/wangsamu' target='_blank'>
               Connect
             </a>
           </article>
           {/* Email */}
-          <article className="contact__option">
-            <HiOutlineMail className=".contact__option-icon" />
+          <article className='contact__option'>
+            <HiOutlineMail className='.contact__option-icon' />
             <h4>Email</h4>
             <h5>mail@wangsamu.com</h5>
-            <a href="mailto:mail@wangsamu.com" target="_blank">
+            <a href='mailto:mail@wangsamu.com' target='_blank'>
               Send an email
             </a>
           </article>
         </div>
 
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder="Your Name" required />
-          <input type="email" name="email" placeholder="Your Email" required />
+          <input type='text' name='name' placeholder='Your Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
           <textarea
-            name="message"
-            rows="7"
-            placeholder="Your Mesasage"
+            name='message'
+            rows='7'
+            placeholder='Your Mesasage'
             required
           ></textarea>
-          <button type="submit" className="btn btn-primary">
+          <button type='submit' className='btn btn-primary'>
             Send Message
           </button>
         </form>
-      </div>
+      </ContactContainer>
     </section>
   );
 };
---
+++ src/Components/contact/ContactStyled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ContactContainer = styled.div`
+  width: 58%;
+  display: grid;
+  grid-template-columns: 30% 58%;
+  gap: 12%;
+`;
+
+export { ContactContainer };
